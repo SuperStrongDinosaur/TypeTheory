4376de77a7655af9dd3158f0e6022295f63aa488
SuperStrongDinosaur <alex_shirv@mail.ru>
2018-10-22 17:49:12 +0300

    LUL

--- hw1_reduction.ml
+++ hw1_reduction.ml
@@ -50,10 +50,65 @@ let rec normal_beta_reduction x = match x with
 			App(l_new, normal_beta_reduction r)	 
 	| Abs(x, r) -> Abs(x, normal_beta_reduction r)
 
-let rec reduce_to_normal_form x = 
-	if is_normal_form x then 
-        x else 
-        reduce_to_normal_form (normal_beta_reduction x);;
+
+let cnt = ref 0;;
+let map_ref = StringMap.empty;;
+
+type lambda_ref = VarRef of string | AbsRef of string * lambda_ref ref | AppRef of lambda_ref ref * lambda_ref ref;; 
+
+let rec ref_from_lambda a = match a with
+    Var x -> ref (VarRef x)
+    | App (x, y) -> ref (AppRef (ref_from_lambda x, ref_from_lambda y))
+    | Abs (x, y) -> ref (AbsRef (x, ref_from_lambda y));;
+
+let rec lambda_from_ref a_ref = match !a_ref with
+    VarRef x -> Var x
+    | AppRef (x, y) -> App (lambda_from_ref x, lambda_from_ref y)
+    | AbsRef (x, y) -> Abs (x, lambda_from_ref y);;
+
+let gen_fresh_name () = 
+    cnt := !cnt + 1;
+    "t" ^ (string_of_int !cnt );;
+
+let rename a = 
+    let rec rename_t a map = match a with
+        Var x -> if StringMap.mem x map then Var (StringMap.find x map) else Var x
+        | App (x, y) -> App (rename_t x map, rename_t y map)
+        | Abs (x, y) ->  let new_var = gen_fresh_name () in Abs (new_var, rename_t y (StringMap.add x new_var map)) in
+    rename_t a map_ref;;
+
+let is_term_beta_redex_ref a_ref = match !a_ref with
+    AbsRef (_, _) -> false
+    | VarRef x -> false
+    | AppRef (x, y) -> match !x with
+        AbsRef (_, _) -> true
+        | _ -> false;;
+
+let rec is_normal_form_ref a_ref = 
+    if is_term_beta_redex_ref a_ref then false else match !a_ref with
+    | VarRef x -> true
+    | AppRef (x, y) -> (is_normal_form_ref x) && (is_normal_form_ref y)
+    | AbsRef (x, y) -> is_normal_form_ref y;;
+
+let rec subst_ref a b var = match !b with
+    VarRef x -> if x = var then b := (!a)
+    | AbsRef (x, y) -> if x != var then subst_ref a y var
+    | AppRef (x, y) -> (subst_ref a x var); (subst_ref a y var);;
+
+let rec normal_beta_reduction_ref a_ref = match !a_ref with
+    VarRef x -> ()
+    | AbsRef (x, y) -> normal_beta_reduction_ref y
+    | AppRef (x, y) -> match !x with
+        AbsRef (o, z) -> a_ref := (!(ref_from_lambda (rename (lambda_from_ref z)))); subst_ref y a_ref o
+        | _ -> if is_normal_form_ref x then (normal_beta_reduction_ref y) else (normal_beta_reduction_ref x);;
+
+let reduce_to_normal_form a = 
+    let rec reduce_to_normal_form_ref a_ref = 
+    if is_normal_form_ref a_ref then 
+        a_ref 
+    else   
+        reduce_to_normal_form_ref (normal_beta_reduction_ref a_ref; a_ref) in
+    lambda_from_ref (reduce_to_normal_form_ref (ref_from_lambda (rename a)));;
 
 
 
